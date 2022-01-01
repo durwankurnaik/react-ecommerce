e5db5e4bc8b576403e86df94aed2ca8b4883e5f6
Durwankur Naik <ndurwankur14@gmail.com>
2022-01-01 16:01:08 +0530

    did the  admin signin work like it is expected to

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxY2Q5ZTc5YzlhM2ZmMWYxYzBhYWQzMiIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NDA5NzE3NDAsImV4cCI6MTY0MTIzMDk0MH0.GAm8NzUNvHGH1D2hHcBj64OKtnyZmwd8sT-x0qH2AjY";
+const TOKEN = JSON.parse(
+  JSON.parse(localStorage.getItem("persist:root")).userReducer
+).currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
